c34e7b8f1e910d8027cec9ba7bc682c205fef688
Noah Glaser <glaserpower@gmail.com>
2020-08-13 23:10:29 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Barebones chrome extension
+
+This was done as a demo for students to mess with.
---
+++ content.js
@@ -0,0 +1,5 @@
+console.log('I have taken over your computer no!!!!');
+
+const color = '#AA0000';
+document.body.style.backgroundColor = color;
+document.querySelector('.sfbgg').style.backgroundColor = color;
